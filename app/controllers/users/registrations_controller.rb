23d6def879c2c4829b1149d609d45ef5e0bdd1d6
EC2 Default User <ec2-user@ip-172-31-26-219.us-east-2.compute.internal>
2020-07-03 16:55:00 +0000

    improve navbar and footer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,7 +51,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :user_adress, :user_zip, :user_city])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :user_adress, :user_zip, :user_city, :deleted_at])
   end
 
 
